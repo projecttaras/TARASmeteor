79e609036a5d7b6613f4c5573d9f2e411b9fb2f4
acemaster <vivekhtc25@gmail.com>
2016-03-14 03:39:09 +0530

    Tinkering with android build not successful

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  id: 'com.epics.taras',
+  name: 'TARAS',
+  description: 'Accident prevention',
+  author: 'EPICS',
+  email: 'vivekhtc25@gmail.com',
+  website: 'http://projecttaras.github.io'
+});
+
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
